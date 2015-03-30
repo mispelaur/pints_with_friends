ff212856f6daeacd0e32bbe97c4f5bd6f5d1b4c0
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-03-30 16:00:00 +0100

    welcome/index linked to and displaying gmap

--- app/assets/javascripts/map_initialize.js
+++ app/assets/javascripts/map_initialize.js
@@ -1,1 +1,17 @@
 console.log("hello, lauren. you're liked to map_initialize.js");
+
+var map;
+
+function initialize() {
+
+  var mapOptions = {
+    center: { lat: -34.397, lng: 150.644},
+    zoom: 13 
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
