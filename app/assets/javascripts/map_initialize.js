17082b750bc757063162a1dc9e34397c9077efb4
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-03-30 16:15:34 +0100

    HTML5 geolocation to center map on user implemented, defaults to London if coords not available

--- app/assets/javascripts/map_initialize.js
+++ app/assets/javascripts/map_initialize.js
@@ -5,12 +5,29 @@ var map;
 function initialize() {
 
   var mapOptions = {
-    center: { lat: -34.397, lng: 150.644},
     zoom: 13 
   };
 
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+  //gets browser geolocation and sets it to center of the map
+  if(navigator.geolocation) {
+    console.log("broswer supports geolocation, centering map on user");
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map.setCenter(pos);
+    }, function() {
+      // Browser support Geolocation, but latlng unavailable
+      console.log("broswer supports geolocation, but it's turned off")
+      setCenterNoGeoloc();
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    console.log("broswer doesn't support geolocation");
+    var nosupportpos = new google.maps.LatLng(51.512802, -0.091324);                     
+    map.setCenter(nosupportpos);
+  }
 
 }
 
